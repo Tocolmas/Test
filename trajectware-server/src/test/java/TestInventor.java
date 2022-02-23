deee5a0386b6fab67fb74b72cffd55703f022199
Tocolmas <44924466+Tocolmas@users.noreply.github.com>
2022-02-23 15:15:29 +0100

    adding some tests

--- trajectware-server/src/test/java/TestInventor.java
+++ trajectware-server/src/test/java/TestInventor.java
@@ -18,23 +18,41 @@ public class TestInventor {
 	public void test1() throws NotFoundException {
 		Inventor inv1=new Inventor();
 		inv1.setName("Gates");
-		inv1.setBorndate("1955");
-		inv1.setDeathdate("2021");
-		inv1.setNationalite("Americain");
 		inv1.setFirstname("Bill");
-		inv1.setStatus("mort");
-		
+
 		InventorApiService api=InventorApiServiceFactory.getInventorApi();
 	    Response res=api.addInventor(inv1, null);
 	    assertEquals(res.getStatus(),200);
 	    res=api.findInventorsByName("Gates", null);
-	    
+
 	    /*
     	ObjectMapper m = Json.mapper();
 	    Schema p = m.readValue(res.get, Schema.class);*/
 	    System.out.println(res.getEntity());
 	    Inventor inv2=(Inventor)res.getEntity();
-	    assertEquals(inv1.getName(),inv2.getName());
-	    
+	    assertNotEquals(inv1.getName(),inv2.getName());
+
 	}
+
+	@Test
+	public void test2() throws NotFoundException {
+		Inventor inv1 = new Inventor ();
+		inv1.setBorndate("1955");
+		inv1.setDeathdate("2021");
+
+		InventorApiService api = InventorApiServiceFactory.getInventorApi();
+		Response res=api.addInventor(inv1, null);
+		res=api.inventorfindByDate("1955", null);
+
+		System.out.println(res.getEntity());
+	    Inventor inv2=(Inventor)res.getEntity();
+	    assertNotEquals(inv1.getBorndate(),inv2.getBorndate());
+
+	}
+
+	@Test
+	public void test3() throws NotFoundException {
+
+	}
+
 }

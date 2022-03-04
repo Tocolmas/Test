9c8e42dc6af714e886dadecf565d811fe924ff7e
Tocolmas <44924466+Tocolmas@users.noreply.github.com>
2022-03-04 16:30:49 +0100

    update event + adding assert

--- Trajectware-server V3/src/main/java/io/swagger/api/impl/EventApiServiceImpl.java
+++ Trajectware-server V3/src/main/java/io/swagger/api/impl/EventApiServiceImpl.java
@@ -3,6 +3,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -12,6 +14,7 @@ import io.swagger.model.*;
 import io.swagger.model.Event;
 import io.swagger.model.Inventor;
 
+import java.util.Date;
 import java.util.List;
 import java.io.InputStream;
 
@@ -19,29 +22,56 @@ import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response.Status;
 import javax.validation.constraints.*;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2022-02-21T11:04:43.972Z")
 public class EventApiServiceImpl extends EventApiService {
+	public void checkValidDate(String s) {
+        assert  s!= null;
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            Date date = format.parse ( s );
+        } catch (ParseException e) {
+            assert false;
+        }
+    }
     @Override
     public Response addevent(Event events, SecurityContext securityContext) throws NotFoundException {
-    	String query = "INSERT INTO Event(Name, Startdate, Enddate) VALUES(?,?,?)";
+    	String query = "INSERT INTO Event(Name, EventId, Startdate, Enddate, Status) VALUES(?,?,?,?,?)";
     	Connection conn = ConnectionManager.getConnection();
+    	long id=ConnectionManager.createId("Event");
+        events.setId(id);
+        assert events.getName() == null;
+        assert events.getId() == null;
+        assert events.getStartdate() == null;
+        assert events.getEnddate() == null;
+        assert events.getStatus() == null;
 	    try (PreparedStatement pstmt = conn.prepareStatement(query)) {
 	        pstmt.setString(1, events.getName());
-	        pstmt.setString(2, events.getStartdate());
-	        pstmt.setString(3, events.getEnddate());
+	        pstmt.setLong(2, events.getId());
+	        pstmt.setString(3, events.getStartdate());
+	        pstmt.setString(4, events.getEnddate());
 	        pstmt.executeUpdate();
 	        pstmt.close();
 	    } catch (SQLException e) {
 	        e.printStackTrace();
+	        ResponseBuilder builder = Response.status(Status.BAD_REQUEST);
+            builder.entity(ConnectionManager.buildException(e));
+            return builder.build();
 	    }
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "new event added")).build();
+	   	assert events.getName() != null && !events.getName().trim().isEmpty();
+	    assert events.getStartdate() != null && !events.getStartdate().trim().isEmpty();
+	   	checkValidDate(events.getStartdate());
+		checkValidDate(events.getEnddate());
+		return Response.ok().entity(events).build();
     }
 
     @Override
     public Response deleteEvent(Long eventId, String apiKey, SecurityContext securityContext) throws NotFoundException {
        	String query = "DELETE FROM Event WHERE EventId = ?";
+       	assert eventId != null;
        	Connection conn = ConnectionManager.getConnection();
     	try (PreparedStatement pstmt = conn.prepareStatement(query)) {
   	        pstmt.setLong(1, eventId);
@@ -49,6 +79,9 @@ public class EventApiServiceImpl extends EventApiService {
   	        pstmt.close();
   	    } catch (SQLException e) {
   	        e.printStackTrace();
+	        ResponseBuilder builder = Response.status(Status.BAD_REQUEST);
+            builder.entity(ConnectionManager.buildException(e));
+            return builder.build();
   	    }
         return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "event deleted!")).build();
     }
@@ -56,81 +89,87 @@ public class EventApiServiceImpl extends EventApiService {
     public Response eventfindByDate( String date, SecurityContext securityContext) throws NotFoundException {
     	String query = "SELECT Name, EventId, Startdate, Enddate FROM Event WHERE Startdate = ?";
 		Event eve = new Event();
+		assert date != null;
+		checkValidDate(date);
 		Connection conn = ConnectionManager.getConnection();
     	try (PreparedStatement preparedStmt = conn.prepareStatement(query)){
     		preparedStmt.setString(1, date);
 			ResultSet rst = preparedStmt.executeQuery();
-			System.out.println("tName\t\tEventId\t\tStartdate\t\tEnddate\n");
+			//System.out.println("tName\t\tEventId\t\tStartdate\t\tEnddate\n");
 			rst.next();
 			eve.setName(rst.getString(1));
 			eve.setId(rst.getLong(2));
 			eve.setStartdate(rst.getString(3));
 			eve.setEnddate(rst.getString(4));
 
-		    System.out.print("\t\t\t"+rst.getString(1));
+		    /*System.out.print("\t\t\t"+rst.getString(1));
 		    System.out.print("\t\t\t"+rst.getLong(2));
 		    System.out.print("\t\t\t"+rst.getString(3));
 		    System.out.print("\t\t\t"+rst.getString(4));
-		    System.out.println();
+		    System.out.println();*/
 
-		    //rst.close();
-		    //preparedStmt.close();
 			}catch (SQLException e) {
-		        System.out.println(e.getMessage());
+		        e.printStackTrace();
+		        ResponseBuilder builder = Response.status(Status.BAD_REQUEST);
+	            builder.entity(ConnectionManager.buildException(e));
+	            return builder.build();
 			}
+    		assert date == eve.getStartdate();
 	    	return Response.ok(eve).build();
 	    }
     @Override
     public Response findEventsByInvention( @NotNull List<String> event, SecurityContext securityContext) throws NotFoundException {
-    	String query = "SELECT * FROM (SELECT Name, EventId, Startdate, Enddate FROM Event WHERE EventId = ? UNION SELECT EventId, EntityId FROM Participation WHERE EventId = ? AND EntityId = ? UNION SELECT EntityId FROM Invention Where EntityId = ? ORDER BY EventId";
+    	String query = "(SELECT * FROM Event UNION SELECT * FROM Participation) AS EP UNION SELECT * FROM Invention WHERE EntityId.EP = EntityId.Invention";
     	Event eve = new Event();
     	Connection conn = ConnectionManager.getConnection();
     	try (PreparedStatement preparedStmt = conn.prepareStatement(query)){
 		ResultSet rst = preparedStmt.executeQuery();
-		System.out.println("tName\t\tEventId\t\tStartdate\t\tEnddate\n");
+		//System.out.println("tName\t\tEventId\t\tStartdate\t\tEnddate\n");
 		rst.next();
 		eve.setName(rst.getString(1));
 		eve.setId(rst.getLong(2));
 		eve.setStartdate(rst.getString(3));
 		eve.setEnddate(rst.getString(4));
 
-	    System.out.print("\t\t\t"+rst.getString(1));
+	    /*System.out.print("\t\t\t"+rst.getString(1));
 	    System.out.print("\t\t\t"+rst.getLong(2));
 	    System.out.print("\t\t\t"+rst.getString(3));
 	    System.out.print("\t\t\t"+rst.getString(4));
-	    System.out.println();
+	    System.out.println();*/
 
-	    //rst.close();
-	    //preparedStmt.close();
 		}catch (SQLException e) {
 	        e.printStackTrace();
+	        ResponseBuilder builder = Response.status(Status.BAD_REQUEST);
+            builder.entity(ConnectionManager.buildException(e));
+            return builder.build();
 		}
     	return Response.ok(eve).build();
 	}
     @Override
     public Response findEventsByInventor( @NotNull List<String> event, SecurityContext securityContext) throws NotFoundException {
-    	String query = "SELECT * FROM (SELECT Name, EventId, Startdate, Enddate FROM Event WHERE EventId = ? UNION SELECT EventId, EntityId FROM Participation WHERE EventId = ? and EntityId = ? UNION SELECT EntityId FROM INVENTOR WHERE EntityId = ? ORDER BY EventId";
+    	String query = "(SELECT * FROM Event UNION SELECT * FROM Participation) AS PE UNION SELECT * FROM Inventor WHERE EntityId.EP = EntityId.Inventor";
     	Event eve = new Event();
     	Connection conn = ConnectionManager.getConnection();
 		try (PreparedStatement preparedStmt = conn.prepareStatement(query)){
 		ResultSet rst = preparedStmt.executeQuery();
-		System.out.println("tName\t\tEventId\t\tStartdate\t\tEnddate\n");
+		//System.out.println("tName\t\tEventId\t\tStartdate\t\tEnddate\n");
 		rst.next();
 		eve.setName(rst.getString(1));
 		eve.setId(rst.getLong(2));
 		eve.setStartdate(rst.getString(3));
 		eve.setEnddate(rst.getString(4));
 
-	    System.out.print("\t\t\t"+rst.getString(1));
+	    /*System.out.print("\t\t\t"+rst.getString(1));
 	    System.out.print("\t\t\t"+rst.getLong(2));
 	    System.out.print("\t\t\t"+rst.getString(3));
 	    System.out.print("\t\t\t"+rst.getString(4));
-	    System.out.println();
+	    System.out.println();*/
 
-//	    rst.close();
-//	    preparedStmt.close();
 		}catch (SQLException e) {
 	        e.printStackTrace();
+	        ResponseBuilder builder = Response.status(Status.BAD_REQUEST);
+            builder.entity(ConnectionManager.buildException(e));
+            return builder.build();
 		}
 		return Response.ok(eve).build();
     }
@@ -142,52 +181,57 @@ public class EventApiServiceImpl extends EventApiService {
     	try (PreparedStatement preparedStmt = conn.prepareStatement(query)){
     		preparedStmt.setString(1, name);
 			ResultSet rst = preparedStmt.executeQuery();
-			System.out.println("tName\t\tEventId\t\tStartdate\t\tEnddate\n");
+			//System.out.println("tName\t\tEventId\t\tStartdate\t\tEnddate\n");
 			rst.next();
 			eve.setName(rst.getString(1));
 			eve.setId(rst.getLong(2));
 			eve.setStartdate(rst.getString(3));
 			eve.setEnddate(rst.getString(4));
 
-		    System.out.print("\t\t\t"+rst.getString(1));
+		    /*System.out.print("\t\t\t"+rst.getString(1));
 		    System.out.print("\t\t\t"+rst.getLong(2));
 		    System.out.print("\t\t\t"+rst.getString(3));
 		    System.out.print("\t\t\t"+rst.getString(4));
-		    System.out.println();
+		    System.out.println();*/
 
-		    //rst.close();
-		    //preparedStmt.close();
 			}catch (SQLException e) {
 		        e.printStackTrace();
+		        ResponseBuilder builder = Response.status(Status.BAD_REQUEST);
+	            builder.entity(ConnectionManager.buildException(e));
+	            return builder.build();
 			}
+    		assert name == eve.getName();
 	    	return Response.ok(eve).build();
 	    }
     @Override
     public Response getEventById(Long eventId, SecurityContext securityContext) throws NotFoundException {
     	String query = "SELECT Name, EventId, Startdate, Enddate FROM Event WHERE EventId = ?";
     	Event eve = new Event();
+    	assert eventId != null;
     	Connection conn = ConnectionManager.getConnection();
 		try (PreparedStatement preparedStmt = conn.prepareStatement(query)){
 			preparedStmt.setLong(1, eventId);
 			ResultSet rst = preparedStmt.executeQuery();
-			System.out.println("tName\t\tEventId\t\tStartdate\t\tEnddate\n");
+			//System.out.println("tName\t\tEventId\t\tStartdate\t\tEnddate\n");
 			rst.next();
 			eve.setName(rst.getString(1));
 			eve.setId(rst.getLong(2));
 			eve.setStartdate(rst.getString(3));
 			eve.setEnddate(rst.getString(4));
 
-		    System.out.print("\t\t\t"+rst.getString(1));
+		    /*System.out.print("\t\t\t"+rst.getString(1));
 		    System.out.print("\t\t\t"+rst.getLong(2));
 		    System.out.print("\t\t\t"+rst.getString(3));
 		    System.out.print("\t\t\t"+rst.getString(4));
-		    System.out.println();
+		    System.out.println();*/
 
-		    //rst.close();
-		    //preparedStmt.close();
 			}catch (SQLException e) {
 		        e.printStackTrace();
+		        ResponseBuilder builder = Response.status(Status.BAD_REQUEST);
+	            builder.entity(ConnectionManager.buildException(e));
+	            return builder.build();
 			}
+		    assert eventId == eve.getId();
 			return Response.ok(eve).build();
 	    }
     @Override
@@ -197,18 +241,26 @@ public class EventApiServiceImpl extends EventApiService {
     }
     @Override
     public Response updateevent(Event events, SecurityContext securityContext) throws NotFoundException {
-    	String query = "UPDATE Event SET Name = ?, Startdate = ?, Enddate = ? WHERE EventId = ?";
-
-		try (Connection conn = ConnectionManager.getConnection();
-				PreparedStatement preparedStmt = conn.prepareStatement(query)){
+    	String query = "UPDATE Event SET Name = ?, Startdate = ?, Enddate = ?, Status = ? WHERE EventId = ?";
+    	assert events.getId() != null;
+    	Connection conn = ConnectionManager.getConnection();
+		try (PreparedStatement preparedStmt = conn.prepareStatement(query)){
 	    preparedStmt.setString(1, events.getName());
 	    preparedStmt.setString(2, events.getStartdate());
-	    preparedStmt.setString(2, events.getEnddate());
+	    preparedStmt.setString(3, events.getEnddate());
+	    preparedStmt.setString(4, events.getStatus());
 	    preparedStmt.executeUpdate();
-	    //preparedStmt.close();
+
 		}catch (SQLException e) {
 	        e.printStackTrace();
+	        ResponseBuilder builder = Response.status(Status.BAD_REQUEST);
+            builder.entity(ConnectionManager.buildException(e));
+            return builder.build();
 		}
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "event updated!")).build();
+		assert events.getName() != null;
+		assert events.getStartdate() != null;
+	   	checkValidDate(events.getStartdate());
+		checkValidDate(events.getEnddate());
+		return Response.ok().entity(events).build();
     }
 }
